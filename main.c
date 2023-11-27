38ad57f7b9acdc64e5c00955e31b21e81b9fce32
Juan Bindez <juanbindez780@gmail.com>
2023-11-27 00:05:38 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <exif-data.h>
+
+void print_tag(ExifIfd ifd, ExifTag tag, ExifContent *content) {
+    ExifEntry *entry = exif_content_get_entry(content, tag);
+    if (entry) {
+        char buf[1024];
+        exif_entry_get_value(entry, buf, sizeof(buf));
+
+        printf("Tag: %s, Value: %s\n", exif_tag_get_name(tag), buf);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    const char *filename = argv[1];
+
+    ExifData *exif = exif_data_new_from_file(filename);
+    if (exif == NULL) {
+        fprintf(stderr, "Failed to read EXIF data from %s\n", filename);
+        return 1;
+    }
+
+    ExifContent *content = exif_data_get_content(exif);
+
+    print_tag(EXIF_IFD_0, EXIF_TAG_MAKE, content);
+    print_tag(EXIF_IFD_0, EXIF_TAG_MODEL, content);
+    print_tag(EXIF_IFD_EXIF, EXIF_TAG_DATE_TIME_ORIGINAL, content);
+
+    exif_data_free(exif);
+
+    return 0;
+}
